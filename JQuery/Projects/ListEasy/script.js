bc985aabb3bc4a5d072d31f09e7a6d270e5eb4ae
Mark Jones <markjoneschiro@gmail.com>
2017-02-08 08:58:54 +0000

    Create script.js

---
+++ JQuery/Projects/ListEasy/script.js
@@ -0,0 +1,48 @@
+var template = function(text) {
+  return '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
+};
+
+var add = function (item) {
+  	var html = template(item);
+      $('.list').append(html);
+};
+
+var main = function() {
+  $('form').submit(function() {
+  var text = $('#todo').val();
+	var html = template(text);
+    $('.list').append(html);
+    $('#todo').val('');
+    return false;  
+  });
+  
+  };
+$(document).on('click','.glyphicon-star', function() {
+             $(this).toggleClass('active');              });
+$(document).on('click','.glyphicon-remove', function() {
+             $(this).parent().remove();              });
+
+
+var showFlickr = function(term) {
+  var url = 'http://api.flickr.com/services/rest/?tags='+tag;
+  $.getJSON(url);
+};
+
+//if (annyang) {
+
+var commands = {
+  // Let's define our first command. First the command we expect, and then the function it should call   
+  // annyang will capture anything after a splat (*) and pass it to the function.
+  // e.g. saying "Show me Batman and Robin" is the same as calling showFlickr('Batman and Robin');
+  'add *item': add,
+};
+
+
+// Add our commands to annyang
+annyang.addCommands(commands);
+
+// Start listening
+annyang.start();
+          
+
+$(document).ready(main);
